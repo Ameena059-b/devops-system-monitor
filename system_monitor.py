dec9e95f82233f22e582ffe6024c615f6e18c2e7
Ameena059-b <bameena059@gmail.com>
2026-03-26 13:06:08 +0530

    Add files via upload

--- system_monitor.py
+++ system_monitor.py
@@ -2,26 +2,21 @@ import os
 import platform
 import socket
 from datetime import datetime
-def system_info():
-print("----SYSTEM INFO---")
+print("----SYSTEM INFO----")
 now = datetime.now()
 print("Date & Time:",now.strftime("%Y-%m-%d  %H:%M:%S"))
 
 hostname=socket.gethostname()
-print("Hostname",hostname)
+print("Hostname:",hostname)
 print("python version:",platform.python_version())
-user =os.getenv("USER") or os.getenv("USERNAME"))
-printf("current user:",user)
-files=os.listdir(",")
-print(""\nFiles in irectory")
+user =os.getenv("USER") or os.getenv("USERNAME")
+print("current user:",user)
+files=os.listdir(".")
+print("\nFiles in directory")
 for file in files:
-print("-",fiile)
+ print("-",file)
 print("\nTotal Files:",len(files))
 
-def user_input_feature():
-print("/n--USER INPUT--")
 name=input("Enter your name:")
-print(f"Hello,{name}! Welcome to the system monitoring tool")
-if_name_=="_main_":
-system_info()
-user_input_feature()
+print("Hello ! Welcome to the system monitoring tool")
+
